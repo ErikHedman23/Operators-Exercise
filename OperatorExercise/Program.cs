5d75a70f82003e203c74dbeb7bdf2410c0fbbe44
Erik Hedman <erikdavid.hedman21@outlook.com>
2023-07-24 12:37:58 -0400

    Completed the two excercises

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -1,10 +1,62 @@
-﻿namespace OperatorExercise
+﻿using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+
+namespace OperatorExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            double operations = Add(1,2);
+            Console.WriteLine(operations);
+            
+            
+            Operators();
+
+            double area = AreaOfCircle();
+            
+            
+           
+
         }
+
+        public static double Add(double a, double b)
+        {
+
+            return a + b;
+           
+        }
+        public static void Operators()
+        {
+            int a = 17;
+            int b = 4;
+            int remainder = a % b;
+            int quotient = a / b;
+
+            Console.WriteLine(a - b);
+            Console.WriteLine(a * b);
+            Console.WriteLine(a / b);
+            Console.WriteLine(++a + b);
+            Console.WriteLine(quotient);
+            Console.WriteLine(remainder);
+            Console.WriteLine($"17/4 = {quotient} with a remainder of {remainder}.");
+
+          }
+
+        public static double AreaOfCircle()
+        {
+            Console.WriteLine("What is the radius of your circle?");
+            var radius = double.Parse(Console.ReadLine());
+            double area = Math.PI* Math.Pow(radius, 2);
+            Console.WriteLine($"The area of your circle is {area}");
+            return area;
+
+
+        }
+
+            
+        }
+
+
     }
-}
+
